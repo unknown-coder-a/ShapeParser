f42277006b4038ac1842b275c6dfc6e8bf8165ae
thhongg <95107864+thhongg@users.noreply.github.com>
2023-05-23 17:22:49 +0700

    Revert "first commit"
    This reverts commit ab16a4ce59967dbf47b569f30225d9bf8f1c056d.

--- ShapeParser/Shape.cpp
+++ ShapeParser/Shape.cpp
@@ -1,9 +1,1 @@
 #include "Shape.h"
-
-Shape::Shape() {
-	_printer = nullptr;
-}
-
-void Shape::setPrinter(std::shared_ptr<IPrint> p) {
-	_printer = p;
-}
--- ShapeParser/Shape.h
+++ ShapeParser/Shape.h
@@ -1,15 +1,5 @@
 #pragma once
-#include <memory>
-#include "IPrint.h"
-class Shape : public IPrint
+class IShape
 {
-private:
-	std::shared_ptr<IPrint> _printer;
-public:
-	Shape();
-
-	virtual float area() = 0;
-	virtual float perimeter() = 0;
-	virtual void setPrinter(std::shared_ptr<IPrint> p);
 };
 
